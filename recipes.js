40401fbddc2f0f8681153aa5726706e9f24bc159
nullpikt0 <140679464+nullpikt0@users.noreply.github.com>
2023-07-27 18:36:18 +0900

    Update recipes.js

--- recipes.js
+++ recipes.js
@@ -13,7 +13,8 @@ function getCSV(){
 // 読み込んだCSVデータを二次元配列に変換する関数convertCSVtoArray()の定義
 function convertCSVtoArray(str){ // 読み込んだCSVデータが文字列として渡される
     var result = str.split(','); // 改行を区切り文字として行を要素とした配列を生成 
-    console.log(result); 
+    return result; 
 }
  
-getCSV(); //最初に実行される
+//最初に実行される
+document.getElementById("test").insertAdjacentHTML("beforeend","<div>" + getCSV() + "</div>");
